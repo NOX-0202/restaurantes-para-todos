a45128bd5085076a253079123a528ee91ec78999
Fernando Oliveira <fernandinho.nco@gmail.com>
2019-08-08 23:09:02 -0300

    header, form and receive data done

---
+++ src/recebe.php
@@ -0,0 +1,10 @@
+<?php
+
+	$obeso   = isset($_POST["obeso"]);
+	$defvisu = isset($_POST["defvisu"]);
+	$defcad  = isset($_POST["defcad"]);
+	$defaud  = isset($_POST["defaud"]);
+	$localidade = $_POST["localidade"];
+
+
+?>
