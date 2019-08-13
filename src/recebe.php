92ee95801af89d515ac1fe9c03bcb5f2c138ab26
root <fernandinho.nco@gmail.com>
2019-08-13 12:43:55 -0500

    search done

--- src/recebe.php
+++ src/recebe.php
@@ -7,34 +7,41 @@
 	$localidade = $_POST["localidade"];
 
 	
-	function pesquisa($resultado){
+	function pesquisa($sql, $con){
 
-		while ($row = mysqli_fetch_assoc($resultado)){
+		$result = mysqli_query($con, $sql);
 
-			echo "teste";
+		while ($rows = mysqli_fetch_assoc($result)){
 
+		 	echo $rows['id'];
+            echo $rows['nome'];
+            echo $rows['local'];
+            echo $rows['obeso'];
+            echo $rows['def_visual'];
+            echo $rows['def_cadeirante'];
+            echo $rows['def_auditivo'];
 
 		}
-
-
 	}
-
-
-
-
-
 
 
 	switch ($deficiencias){
 		case "obeso":
-			$query = "SELECT * FROM RESTAUTANTES WHERE deficiencia = '$deficiencias' AND local = $localidade";
-			$result = mysqli_query($conn, $query);
-			pesquisa($result);
+			$query = "SELECT * FROM restaurantes WHERE obeso = '$deficiencias' AND local = '$localidade'";
+			pesquisa($query, $conn);
 			break;
-		
-		case "teste":
-
-
+		case "def-visu":
+			$query = "SELECT * FROM restaurantes WHERE def_visual = '$deficiencias' AND local = '$localidade'";
+			pesquisa($query, $conn);
+			break;
+		case "cadeirante":
+			$query = "SELECT * FROM restaurantes WHERE def_cadeirante = '$deficiencias' AND local = '$localidade'";
+			pesquisa($query, $conn);
+			break;
+		case "def-auditivo":
+			$query = "SELECT * FROM restaurantes WHERE def_auditivo = '$deficiencias' AND local = '$localidade'";
+			pesquisa($query, $conn);
+			break;
 	}
 
 
